592822aed34b5e304532e9db40b0ad2b310f5efe
George Allman <George.allman777@gmail.com>
2026-04-19 20:09:23 +1000

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -701,7 +701,7 @@ function App() {
           <div className="flex flex-col justify-center">
             <Button
               variant="secondary"
-              className="w-[22vw] h-full"
+              className="text-2xl w-[22vw] h-full"
               onClick={handleRunPipeline}
               disabled={!selectedCity || !businessCategory || isPipelineRunning}
             >
@@ -922,61 +922,6 @@ function App() {
                 ))}
               </TableBody>
             </Table>
-          </div>
-          <div>
-            <Card className="w-full h-full">
-              <CardHeader>
-                <CardTitle>Area Chart</CardTitle>
-                <CardDescription>
-                  Showing total visitors for the last 6 months
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <ChartContainer config={chartConfig}>
-                  <AreaChart
-                    accessibilityLayer
-                    data={chartData}
-                    margin={{
-                      left: 12,
-                      right: 12,
-                    }}
-                  >
-                    <CartesianGrid vertical={false} />
-                    <XAxis
-                      dataKey="month"
-                      tickLine={false}
-                      axisLine={false}
-                      tickMargin={8}
-                      tickFormatter={(value) => value.slice(0, 3)}
-                    />
-                    <ChartTooltip
-                      cursor={false}
-                      content={<ChartTooltipContent indicator="line" />}
-                    />
-                    <Area
-                      dataKey="desktop"
-                      type="natural"
-                      fill="oklch(0.424 0.199 265.638)"
-                      fillOpacity={0.4}
-                      stroke="oklch(0.424 0.199 265.638)"
-                    />
-                  </AreaChart>
-                </ChartContainer>
-              </CardContent>
-              <CardFooter>
-                <div className="flex w-full items-start gap-2 text-sm">
-                  <div className="grid gap-2">
-                    <div className="flex items-center gap-2 leading-none font-medium">
-                      Trending up by 5.2% this month{" "}
-                      <TrendingUp className="h-4 w-4" />
-                    </div>
-                    <div className="flex items-center gap-2 leading-none text-muted">
-                      January - June 2024
-                    </div>
-                  </div>
-                </div>
-              </CardFooter>
-            </Card>
           </div>
         </div>
       </div>
